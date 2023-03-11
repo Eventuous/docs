abea655d8258910c606d77721b264752b99114e5
Alexey Zimarev <alex@zimarev.com>
2023-03-11 12:30:49 +0100

    Check if can use DOM

--- src/theme/DocItem/Metadata/index.js
+++ src/theme/DocItem/Metadata/index.js
@@ -2,21 +2,23 @@ import React from 'react';
 import Metadata from '@theme-original/DocItem/Metadata';
 import {useDoc} from "@docusaurus/theme-common/internal";
 import {useSidebarBreadcrumbs} from "@docusaurus/theme-common/internal";
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
 
 let previous = null;
 
 export default function MetadataWrapper(props) {
-    if (window.analytics) return;
+    if (ExecutionEnvironment.canUseDOM && window.analytics) {
 
-    const x = useDoc();
-    if (previous !== x) {
-        previous = x;
-        const sb = useSidebarBreadcrumbs();
-        const category = sb[0].label;
-        const page = sb[sb.length - 1].label;
-        setTimeout(() => window.analytics.page(page, {category: category}), 0);
-        // console.log('category', category);
-        // console.log('page', page);
+        const x = useDoc();
+        if (previous !== x) {
+            previous = x;
+            const sb = useSidebarBreadcrumbs();
+            const category = sb[0].label;
+            const page = sb[sb.length - 1].label;
+            setTimeout(() => window.analytics.page(page, {category: category}), 0);
+            // console.log('category', category);
+            // console.log('page', page);
+        }
     }
     return (
         <>
